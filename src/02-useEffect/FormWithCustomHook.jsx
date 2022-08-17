80b16c0c361d5b2a74f6bf12164e50998d335bb3
Pedro Gabriel <31576642+PeterGabrielVE@users.noreply.github.com>
2022-08-17 09:44:42 -0400

    formulario con custom Hook

---
+++ src/02-useEffect/FormWithCustomHook.jsx
@@ -0,0 +1,23 @@
+import { useEffect, useState } from "react"
+import { useForm } from "../hooks/useForm";
+
+export const FormWithCustomHook = () => {
+
+ const { formState, onInputChange, username, email, password } = useForm({
+    username: '',
+    email: '',
+    password:'',
+ });
+
+  return (
+    <>
+        <h1>Formulario con Custom Hook</h1>
+        <hr />
+
+        <input type="text" name="username" className="form-control" placeholder="UserName" value={ username } onChange={ onInputChange } />
+        <input type="email" name="email" className="form-control mt-2" placeholder="gaboleal123@gmail.com" value={ email }  onChange={ onInputChange }/>
+        <input type="password" name="password" className="form-control mt-2" placeholder="Contraseña" value={ password } onChange={ onInputChange } />
+        
+    </>
+  )
+}
