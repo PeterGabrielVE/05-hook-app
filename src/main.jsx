e66cdb44a1195508f3e4847bb349a7e3c08a2fb9
Pedro Gabriel <31576642+PeterGabrielVE@users.noreply.github.com>
2022-08-18 18:11:50 -0400

    useFetch custoom hooks

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 //import { FormWithCustomHook } from './02-useEffect/FormWithCustomHook'
-import { MultipleCustomHooks } from './02-useEffect/MultipleCustomHooks'
+import { MultipleCustomHooks } from './03-examples/MultipleCustomHooks'
 //import { CounterApp } from './01-useState/CounterApp'
 //import { CounterWithCustomHook } from './01-useState/CounterWithCustomHook'
 //import { SimpleForm } from './02-useEffect/SimpleForm'
