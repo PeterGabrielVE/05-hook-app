ca44fb5261db51c31d65a49bca14b96768758e6f
Pedro Gabriel <31576642+PeterGabrielVE@users.noreply.github.com>
2022-08-20 13:06:53 -0400

    MemoHook

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { MemoHook } from './06-memos/MemoHook'
 //import { FormWithCustomHook } from './02-useEffect/FormWithCustomHook'
 //import { MultipleCustomHooks } from './03-examples/MultipleCustomHooks'
 //import { Layout } from './05-useLayoutEffect/Layout'
@@ -13,6 +14,6 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Memorize/>
+    <MemoHook/>
   </React.StrictMode>
 )
