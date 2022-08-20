4753d2a7425a38e8eb5a0c1fcd3a63b7106ec7ed
Pedro Gabriel <31576642+PeterGabrielVE@users.noreply.github.com>
2022-08-20 13:22:38 -0400

    useCallback

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { MemoHook } from './06-memos/MemoHook'
+import { CallbackHook } from './06-memos/CallBackHook'
+//import { MemoHook } from './06-memos/MemoHook'
 //import { FormWithCustomHook } from './02-useEffect/FormWithCustomHook'
 //import { MultipleCustomHooks } from './03-examples/MultipleCustomHooks'
 //import { Layout } from './05-useLayoutEffect/Layout'
-import { Memorize } from './06-memos/Memorize'
+//import { Memorize } from './06-memos/Memorize'
 //import { CounterApp } from './01-useState/CounterApp'
 //import { CounterWithCustomHook } from './01-useState/CounterWithCustomHook'
 //import { SimpleForm } from './02-useEffect/SimpleForm'
@@ -14,6 +15,6 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <MemoHook/>
+    <CallbackHook/>
   </React.StrictMode>
 )
