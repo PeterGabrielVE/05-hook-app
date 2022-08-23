4e22a0b19cf0eb7be8cd2b03ab0710a23ffa2942
Pedro Gabriel <31576642+PeterGabrielVE@users.noreply.github.com>
2022-08-22 20:34:59 -0400

    TodoList

---
+++ src/08-useReducer/TodoApp.jsx
@@ -0,0 +1,17 @@
+import React, { useReducer } from 'react'
+
+export const TodoApp = () => {
+
+    const [state, dispatch] = useReducer(first, second)
+  return (
+    <>
+        <h1>TodoApp</h1>
+        <hr />
+        <ul>
+            <li>Item 1</li>
+            <li>Item 2</li>
+            <li>Item 3</li>
+        </ul>
+    </>
+  )
+}
