fe9947f5fe983486172539d97a4d1e169b2c8c7b
Pedro Gabriel <31576642+PeterGabrielVE@users.noreply.github.com>
2022-08-24 11:16:35 -0400

    useTodo

--- src/08-useReducer/TodoApp.jsx
+++ src/08-useReducer/TodoApp.jsx
@@ -1,17 +1,38 @@
-import React, { useReducer } from 'react'
+import { useTodos } from '../hooks';
+import { TodoAdd } from './TodoAdd';
+import { TodoList } from './TodoList';
+
 
 export const TodoApp = () => {
 
-    const [state, dispatch] = useReducer(first, second)
-  return (
-    <>
-        <h1>TodoApp</h1>
-        <hr />
-        <ul>
-            <li>Item 1</li>
-            <li>Item 2</li>
-            <li>Item 3</li>
-        </ul>
-    </>
-  )
+    const { todos, todosCount, pendingTodosCount, handleDeleteTodo, handleToggleTodo, handleNewTodo } = useTodos();
+    
+    return (
+        <>
+            <h1>TodoApp: { todosCount }, <small>pendientes: { pendingTodosCount }</small> </h1>
+            <hr />
+
+            <div className="row">
+                <div className="col-7">
+                    <TodoList
+                        todos={ todos } 
+                        onDeleteTodo={ handleDeleteTodo } 
+                        onToggleTodo={ handleToggleTodo }
+                    />
+                </div>
+
+                <div className="col-5">
+                    <h4>Agregar TODO</h4>
+                    <hr />
+                    <TodoAdd 
+                        onNewTodo={ handleNewTodo }  
+                    />
+                </div>
+
+            </div>
+
+
+        
+        </>
+    )
 }
